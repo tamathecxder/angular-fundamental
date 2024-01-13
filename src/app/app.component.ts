d380558de6b500e1e6259954b71cf243948900fd
Yudistira Eka Pratama <yudhistira.pratama@thebigrichgroup.com>
2024-01-13 20:18:49 +0700

    import those new components into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,20 @@ import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishitem';
 import { FormsModule } from '@angular/forms';
 import { WishListComponent } from './wish-list/wish-list.component';
-
-const filters = [
-  (items: WishItem) => items,
-  (items: WishItem) => !items.isComplete,
-  (items: WishItem) => items.isComplete
-];
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, WishListComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -26,16 +29,9 @@ export class AppComponent {
     new WishItem("Take a nap", true)
   ];
 
-  title = 'Testing';
-  newWishText: string = "";
-  listFilter: any = "0";
+  filter: any = () => {};
 
   get visibleItems(): WishItem[] {
-    return this.items.filter(filters[this.listFilter]);
-  }
-
-  addNewWish() {
-    this.items.push(new WishItem(this.newWishText));
-    this.newWishText = "";
+    return this.items.filter(this.filter);
   }
 }
