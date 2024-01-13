ebfe4feb8c798e1b6fde3e2b49cad7bf4add66a7
Yudistira Eka Pratama <yudhistira.pratama@thebigrichgroup.com>
2024-01-13 15:29:28 +0700

    import FormsModule for ngModel & create new method to create new wish item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,32 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishitem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem("Learn Angular"),
-    new WishItem("Museum Date"),
-    new WishItem("Take a nap", true)
+    // new WishItem("Learn Angular"),
+    // new WishItem("Museum Date"),
+    // new WishItem("Take a nap", true)
   ];
 
   title = 'Testing';
 
+  newWishText: string = "";
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = "";
+  }
+
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
-
-    console.log(item);
   }
 }
