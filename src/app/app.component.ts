14cedf951419b08eeae6cc43a2ddfec2b5f3c375
Yudistira Eka Pratama <yudhistira.pratama@thebigrichgroup.com>
2024-01-13 15:59:22 +0700

    create filter method to processing what is status of data that can be show into the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,32 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem("Learn Angular"),
-    // new WishItem("Museum Date"),
-    // new WishItem("Take a nap", true)
+    new WishItem("Learn Angular"),
+    new WishItem("Museum Date"),
+    new WishItem("Take a nap", true)
   ];
 
   title = 'Testing';
 
   newWishText: string = "";
+  listFilter: string = "0";
+  visibleItems: WishItem[] = this.items;
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = "";
+  }
+
+  filterChanged(value: any) {
+    console.log(value);
+
+    if (value === "0") {
+      this.visibleItems = this.items;
+    } else if (value === "1") {
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else if (value === "2") {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
   }
 
   toggleItem(item: WishItem) {
